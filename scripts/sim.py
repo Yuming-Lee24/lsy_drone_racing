dc8c1b9d69b0d2c13f4e9b2ec6a57360e31c038c
Yuming Li <141678040+Yuming-Lee24@users.noreply.github.com>
2026-07-14 16:48:10 +0200

    Add warning for controller execution time exceeding loop frequency in sim (#119)
    * Add warning for controller execution time exceeding loop frequency in sim
    
    * Apply suggestions from code review
    
    Co-authored-by: Marcel Rath <75042654+ratheron@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Marcel Rath <75042654+ratheron@users.noreply.github.com>

--- scripts/sim.py
+++ scripts/sim.py
@@ -10,6 +10,7 @@ Look for instructions in `README.md` and in the official documentation.
 from __future__ import annotations
 
 import logging
+import time
 from pathlib import Path
 from typing import TYPE_CHECKING
 
@@ -81,13 +82,18 @@ def simulate(
         while True:
             curr_time = i / config.env.freq
 
+            t_loop = time.perf_counter()
             action = controller.compute_control(obs, info)
+            control_time = time.perf_counter() - t_loop
 
             obs, reward, terminated, truncated, info = env.step(action)
             # Update the controller internal state and models.
+            t_callback = time.perf_counter()
             controller_finished = controller.step_callback(
                 action, obs, reward, terminated, truncated, info
             )
+            if (exc := time.perf_counter() - t_callback + control_time - 1 / config.env.freq) > 0:
+                logger.warning(f"Controller execution time exceeded loop frequency by {exc:.3f}s.")
             # Add up reward, collisions
             if terminated or truncated or controller_finished:
                 break
